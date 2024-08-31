bcfbaa582ac00d0dfc7bc8f46aa059ff3e98717c
Baumaro <botero@etrr.edu.ar>
2024-08-31 15:19:52 -0300

    ajedrez

--- Lenguajes electronicos/Actividades c/ajedrez.c
+++ Lenguajes electronicos/Actividades c/ajedrez.c
@@ -49,6 +49,7 @@ int main(void){
         if((tablero[casilla_Mov_y][casilla_Mov_x] == PEON_BLANCO)){
             mov_peon(tablero, casilla_Mov_y, casilla_Mov_x);
         }
+        wprintf(L"%x", tablero[7][6]);
     }
 }
 
@@ -107,36 +108,38 @@ void reset(wchar_t tablero[8][8]) {
     tablero[7][4] = REY_NEGRO;
 }
 
+// en el tablero de la consola los colores se invierten
+
 void mov_peon(wchar_t tablero[8][8], int casilla_Mov_y, int casilla_Mov_x){
-    int comer, comer_direccion, mover_2_casillas;
-    if(((tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] != " " ) || (tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] != " " )) && (((tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > 0x2654 ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > 0x2659 )) || ((tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > 0x2654 ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > 0x2659 ))))
+    int comer, comer_direccion, mover_2_casillas = 0;
+    if(((tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] != ' ' ) || (tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] != ' ' )) && (((tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > REY_NEGRO ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > PEON_NEGRO )) || ((tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > REY_NEGRO ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > PEON_NEGRO ))))
     {
         wprintf(L"desea comer? \n(1) - si \n(0) - no");
         scanf("%d", &comer);
-        if(((tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > 0x2654 ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > 0x2659 )) && (comer == 1)) 
+        if(((tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > REY_NEGRO ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x + 1] > PEON_NEGRO )) && (comer == 1)) 
         {
             wprintf(L"(1) - comer derecha \n(0) - no");
             scanf("%d", &comer_direccion);
-        }else if(((tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > 0x2654 ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > 0x2659 )) && (comer == 1))
+        }else if(((tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > REY_NEGRO ) && (tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] > PEON_NEGRO )) && (comer == 1))
         {
             wprintf(L"(1) - comer izquierda \n(0) - no");
             scanf("%d", &comer_direccion);
             if (comer_direccion == 1)
             {
                 tablero[casilla_Mov_y + 1][casilla_Mov_x - 1] = tablero[casilla_Mov_y][casilla_Mov_x];
-                tablero[casilla_Mov_y][casilla_Mov_x] = " ";
+                tablero[casilla_Mov_y][casilla_Mov_x] = ' ';
             }
             
         }
     }
-    if ((comer != 1) && (((casilla_Mov_x > 0) && (casilla_Mov_x < 8)) && (casilla_Mov_y == 1)))
+    if ((comer != 1) && (((casilla_Mov_x >= 0) && (casilla_Mov_x <= 8)) && (casilla_Mov_y == 1)))
     {
         wprintf(L"desea mover dos casillas? \n(1) - si \n(0) - no\n");
         scanf("%d", &mover_2_casillas);
         if (mover_2_casillas == 1)
         {
             tablero[casilla_Mov_y + 2][casilla_Mov_x] = tablero[casilla_Mov_y][casilla_Mov_x];
-            tablero[casilla_Mov_y][casilla_Mov_x] = " ";
+            tablero[casilla_Mov_y][casilla_Mov_x] = ' ';
             mover_2_casillas = 0;
         }
     }
@@ -144,7 +147,8 @@ void mov_peon(wchar_t tablero[8][8], int casilla_Mov_y, int casilla_Mov_x){
     if ((comer != 1) && (mover_2_casillas != 1))
     {
         tablero[casilla_Mov_y + 1][casilla_Mov_x] = tablero[casilla_Mov_y][casilla_Mov_x];
-        tablero[casilla_Mov_y][casilla_Mov_x] = " ";
+        tablero[casilla_Mov_y][casilla_Mov_x] = ' ';
     }
 }
+
 
