d731789f0dd5729d8d849bd545e6fdf0e2080715
Baumaro <botero@etrr.edu.ar>
2025-03-11 22:54:16 -0300

    herencia

---
+++ Lenguajes-electronicos/Codigos-python/CODIGOS/POO/code_1.py
@@ -0,0 +1,42 @@
+import MODULOS_BAUMARO.Usos_Comunes as MB
+import pandas as pd
+from pathlib import Path
+
+
+
+
+        
+
+
+def main():
+
+    apagar = False
+
+    MB.bienvenida()
+
+    while apagar == False:
+
+
+
+
+
+
+class Producto():
+
+    def __init__(self, Nombre, Precio, Categoria):
+
+
+class Disp_electronico():
+
+class Moda():
+
+class Arte():
+
+class Samsung():
+
+class Apple()
+
+
+if __name__ == "__main__":
+    main()
+
