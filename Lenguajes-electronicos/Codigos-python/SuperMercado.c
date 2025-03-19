513a683ddc9f1a0d4db09fb98d40433edb8de731
Baumaro <botero@etrr.edu.ar>
2025-03-18 22:58:11 -0300

    faawfwa

--- Lenguajes-electronicos/Codigos-python/SuperMercado.c
+++ Lenguajes-electronicos/Codigos-python/SuperMercado.c
@@ -1,8 +1,17 @@
-
+#include <stdio.h>
 
 typedef struct
 {
 
     char Nombre[10];
+    float Precio;
+    int Codigo;
 
 } Producto_t;
+
+int main()
+{
+    int pepe;
+    printf("Hola mUndo");
+    scanf("%d", &pepe);
+}
